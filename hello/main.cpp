989b6129c5c8ba4f11f7ae2039b1e89ebf2709ea
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2019-10-05 10:17:38 +0500

    Initial Commit

---
+++ hello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by Sarfaraz Hussain on 05/10/2019.
+//  Copyright © 2019 Sarfaraz Hussain. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
